5ca071c390ff575af0103b62eee0116e557879da
corykinney <cory.kinney@ucf.edu>
2023-03-09 14:50:26 -0500

    Move `format_mixture` to `utils`

--- knightshock/__init__.py
+++ knightshock/__init__.py
@@ -4,19 +4,6 @@ __version__ = "0.0.1"
 from . import absorption
 from . import figures
 from . import kinetics
+from . import constants
 
-
-def format_mixture(mixture: str | dict) -> dict[str, float]:
-    if isinstance(mixture, dict):
-        return dict((x.strip().upper(), float(y)) for x, y in mixture.items())
-    elif isinstance(mixture, str):
-        mixture = mixture.replace("{", "").replace("}", "").upper()
-        if ":" not in mixture:
-            return {mixture.strip(): 1.0}
-        else:
-            return dict(
-                (x.strip(), float(y))
-                for x, y in (element.split(":") for element in (mixture.split(",")))
-            )
-    else:
-        raise TypeError("Mixture argument must be str or dict.")
+from .utils import format_mixture
